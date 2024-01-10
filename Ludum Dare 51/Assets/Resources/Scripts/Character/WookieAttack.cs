4165879c4b3c23153139166053d5f7e7a7f3f8bd
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:14:25 -0500

    Update WookieAttack.cs
    Clean up

--- Ludum Dare 51/Assets/Resources/Scripts/Character/WookieAttack.cs
+++ Ludum Dare 51/Assets/Resources/Scripts/Character/WookieAttack.cs
@@ -4,48 +4,53 @@ using UnityEngine;
 
 public class WookieAttack : MonoBehaviour
 {
+    [SerializeField] private float startTimeBtwAttack;
+    [SerializeField] private Transform attackPos;
+    [SerializeField] private float AttackRange;
+    [SerializeField] private LayerMask mask;
+    [SerializeField] private int damage;
+    [SerializeField] private Animator animator;
+
     private float timeBtwAttack;
-    [SerializeField]
-    private float startTimeBtwAttack;
-    public Transform attackPos;
-    [SerializeField]
-    private float AttackRange;
-    [SerializeField]
-    private LayerMask mask;
-    public int damage;
-    [SerializeField]
-    private Animator animator;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (timeBtwAttack <= 0)
+        if (timeBtwAttack <= 0 && Input.GetMouseButtonDown(0))
         {
-            if (Input.GetKey(KeyCode.Mouse0))
-            {
-                animator.Play("chomp");
-                Controller controller = FindObjectOfType<Controller>();
-                controller.audioManager.play("Chomp");
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, AttackRange, mask);
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                    enemiesToDamage[i].GetComponent<MonsterManager>().takeDamage(damage);
-                }
-                timeBtwAttack = startTimeBtwAttack;
-            }
+            PerformAttack();
+            timeBtwAttack = startTimeBtwAttack;
         }
         else
         {
             timeBtwAttack -= Time.deltaTime;
         }
     }
-    void OnDrawGizmosSelected() {
+
+    void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(attackPos.position,AttackRange);
+        Gizmos.DrawWireSphere(attackPos.position, AttackRange);
+    }
+
+    void PerformAttack()
+    {
+        animator.Play("chomp");
+        Controller controller = FindObjectOfType<Controller>();
+        if (controller != null)
+        {
+            controller.audioManager.play("Chomp");
+        }
+
+        Collider2D[] enemies = new Collider2D[10]; // Adjust the size based on your needs
+        int enemyCount = Physics2D.OverlapCircleNonAlloc(attackPos.position, AttackRange, enemies, mask);
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            MonsterManager monsterManager = enemies[i].GetComponent<MonsterManager>();
+            if (monsterManager != null)
+            {
+                monsterManager.takeDamage(damage);
+            }
+        }
     }
 }
