7b97d5afb4aefaaaabd9f0a5cffa496e26880104
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:18:13 -0500

    Update Pinwheel.cs
    cleaned up some nasty looking code

--- Ludum Dare 51/Assets/Resources/Scripts/Monsters/Pinwheel.cs
+++ Ludum Dare 51/Assets/Resources/Scripts/Monsters/Pinwheel.cs
@@ -1,36 +1,68 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Pinwheel : MonoBehaviour
 {
     public GameObject[] slices = new GameObject[8];
     public MonsterManager monster;
-    [SerializeField]
-    private float rotateSpeed;
-    [SerializeField]
-    private float flySpeed;
+
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float flySpeed;
+    [SerializeField] private Transform player;
+
     public Vector3 start;
-    [SerializeField]
-    private Transform player;
-    public bool returning;
     private float timetoReturn;
-    [SerializeField]
-    private float maxTimeToReturn;
+    [SerializeField] private float maxTimeToReturn;
     public bool canHit;
     public float sienfield;
     private float timeBtwHits;
-    [SerializeField]
-    private float maxBtwHits;
-    [SerializeField]
-    private float offset;
-    // Start is called before the first frame update
+    [SerializeField] private float maxBtwHits;
+    [SerializeField] private float offset;
+
     public float timeBtwPhase;
     public float maxTimeBtwPhase;
-    [SerializeField]
-    private Slider healthBar;
+
+    [SerializeField] private Slider healthBar;
     public bool idle;
+
     void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        healthBar.value = monster.getCurrentHealth();
+
+        UpdateSlices();
+
+        if (timeBtwPhase < maxTimeBtwPhase)
+        {
+            timeBtwPhase += Time.deltaTime;
+        }
+        else
+        {
+            ChoosePhase();
+        }
+
+        if (idle)
+        {
+            HandleIdle();
+        }
+
+        if (monster.inMelee)
+        {
+            HandleMeleePhase();
+        }
+
+        if (monster.inRanged)
+        {
+            HandleRangedPhase();
+        }
+    }
+
+    void Initialize()
     {
         monster = GetComponent<MonsterManager>();
         start = transform.position;
@@ -40,112 +72,79 @@ public class Pinwheel : MonoBehaviour
         healthBar.value = monster.getCurrentHealth();
     }
 
-    // Update is called once per frame
-    public bool inmelee;
-    public bool inRanged;
-    public int randomnumber;
-
-    void Update()
+    void UpdateSlices()
     {
-        healthBar.value = monster.getCurrentHealth();
-        if (monster.getCurrentHealth() == 70)
+        for (int i = 0; i < slices.Length; i++)
         {
-
-            Destroy(slices[6]);
-        }
-        if (monster.getCurrentHealth() == 60)
-        {
-
-            Destroy(slices[5]);
-        }
-        if (monster.getCurrentHealth() == 50)
-        {
-
-            Destroy(slices[4]);
-        }
-        if (monster.getCurrentHealth() == 40)
-        {
-
-            Destroy(slices[3]);
-        }
-        if (monster.getCurrentHealth() == 30)
-        {
-
-            Destroy(slices[2]);
-        }
-        if (monster.getCurrentHealth() == 20)
-        {
-
-            Destroy(slices[1]);
-        }
-        if (monster.getCurrentHealth() == 10)
-        {
-
-            Destroy(slices[0]);
-        }
-
-
-        if (timeBtwPhase < maxTimeBtwPhase)
-        {
-            timeBtwPhase += Time.deltaTime;
-        }
-        else
-        {
-
-            randomnumber = Random.Range(0, 3);
-            if (randomnumber == 0)
+            if (monster.getCurrentHealth() == (i + 1) * 10)
             {
-                idle = false;
-                inmelee = true;
-                inRanged = false;
-                 timeBtwPhase = 0;
+                Destroy(slices[i]);
             }
-             if (randomnumber == 1)
-            {
-                idle = true;
-                inmelee = false;
-                inRanged = false;
-                timeBtwPhase = maxTimeBtwPhase -2;
-            }
-            if (randomnumber == 2)
-            {
-                inRanged = true;
-                idle = false;
-                inmelee = false;
-                 timeBtwPhase = 0;
-            }
-           
-
-        }
-        if (idle)
-        {
-            monster.animator.Play("Idle");
-            
-            monster.rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
-        }
-        if (inmelee )
-        {
-            spin();
-            meleephase();
-            if (monster.animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !monster.animator.IsInTransition(0))
-                monster.animator.Play("spin");
-            monster.rb.constraints = RigidbodyConstraints2D.None;
-        }
-        if (inRanged )
-        {
-            if (monster.animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !monster.animator.IsInTransition(0))
-                monster.animator.Play("Fire");
-            monster.rb.SetRotation(0);
-            monster.rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         }
     }
-    void DisableMelee()
+
+    void ChoosePhase()
     {
-        inmelee = false;
+        int randomnumber = Random.Range(0, 3);
+        if (randomnumber == 0)
+        {
+            idle = false;
+            monster.inMelee = true;
+            monster.inRanged = false;
+            timeBtwPhase = 0;
+        }
+        else if (randomnumber == 1)
+        {
+            idle = true;
+            monster.inMelee = false;
+            monster.inRanged = false;
+            timeBtwPhase = maxTimeBtwPhase - 2;
+        }
+        else if (randomnumber == 2)
+        {
+            monster.inRanged = true;
+            idle = false;
+            monster.inMelee = false;
+            timeBtwPhase = 0;
+        }
     }
+
+    void HandleIdle()
+    {
+        monster.animator.Play("Idle");
+        monster.rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    void HandleMeleePhase()
+    {
+        spin();
+        meleephase();
+
+        if (monster.animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !monster.animator.IsInTransition(0))
+        {
+            monster.animator.Play("spin");
+        }
+        monster.rb.constraints = RigidbodyConstraints2D.None;
+    }
+
+    void HandleRangedPhase()
+    {
+        if (monster.animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !monster.animator.IsInTransition(0))
+        {
+            monster.animator.Play("Fire");
+        }
+        monster.rb.SetRotation(0);
+        monster.rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    void spin()
+    {
+        monster.rb.rotation += rotateSpeed;
+    }
+
     void meleephase()
     {
-        if (timeBtwHits < maxBtwHits && !returning)
+        if (timeBtwHits < maxBtwHits && !monster.returning)
         {
             timeBtwHits += Time.deltaTime;
             fly();
@@ -155,7 +154,8 @@ public class Pinwheel : MonoBehaviour
             canHit = true;
             swoop();
         }
-        if (returning)
+
+        if (monster.returning)
         {
             if (timetoReturn < maxTimeToReturn)
             {
@@ -165,36 +165,18 @@ public class Pinwheel : MonoBehaviour
             else
             {
                 timetoReturn = 0;
-                returning = false;
-
+                monster.returning = false;
             }
         }
     }
+
     void fly()
     {
         sienfield = Mathf.Sin(Time.realtimeSinceStartup) * offset;
-        if (Mathf.Sin(Time.realtimeSinceStartup) * offset < 0)
-        {
-            monster.rb.velocity = new Vector2(-1, 0) * flySpeed;
-        }
-        else
-        {
-            monster.rb.velocity = new Vector2(1, 0) * flySpeed;
-        }
+        Vector2 velocity = Mathf.Sin(Time.realtimeSinceStartup) * offset < 0 ? new Vector2(-1, 0) * flySpeed : new Vector2(1, 0) * flySpeed;
+        monster.rb.velocity = velocity;
+    }
 
-    }
-    void shoot()
-    {
-        for (int i = 0; i < slices.Length; i++)
-        {
-            if (slices[i] != null)
-                slices[i].GetComponent<sliceBehavior>().shoot();
-        }
-    }
-    void spin()
-    {
-        monster.rb.rotation += rotateSpeed;
-    }
     void returnFly()
     {
         Vector3 dir = (start - monster.rb.transform.position).normalized;
@@ -217,17 +199,16 @@ public class Pinwheel : MonoBehaviour
             monster.rb.MovePosition(monster.rb.transform.position + dir * flySpeed * Time.fixedDeltaTime);
         }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.tag == "Player" && canHit)
         {
-            returning = true;
+            monster.returning = true;
             canHit = false;
             PlayerManager pm = other.GetComponent<PlayerManager>();
-
             pm.loseHealth();
             timeBtwHits = 0;
         }
-
     }
 }
